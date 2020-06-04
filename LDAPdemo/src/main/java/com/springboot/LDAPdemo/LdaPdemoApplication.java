0156ae69fdb1daa2893abe22b7dfce61745e560a
user <user@DESKTOP-STDD4K1>
2020-06-04 11:13:24 -0700

    spring boot with openldap aserver for authentication

---
+++ LDAPdemo/src/main/java/com/springboot/LDAPdemo/LdaPdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.LDAPdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaPdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdaPdemoApplication.class, args);
+	}
+
+}
---
+++ LDAPdemo/src/test/java/com/springboot/LDAPdemo/LdaPdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.LDAPdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdaPdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
